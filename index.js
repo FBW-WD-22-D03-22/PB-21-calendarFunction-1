842e00e89e30b57b36591b88e886b370b8cc7018
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-24 14:46:04 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function nextDay(wochentag){
+
+    if (wochentag == "Montag") {
+         console.log("Dienstag");
+        
+    }
+    if (wochentag == "Dienstag") {
+         console.log("Mittwoch");
+        
+    }
+    if (wochentag == "Mittwoch") {
+         console.log("Donnerstag");
+        
+    }
+    if (wochentag == "Donnerstag") {
+         console.log("Freitag");
+        
+    }
+    if (wochentag == "Freitag") {
+         console.log("Samstag");
+        
+    }
+    if (wochentag == "Samstag") {
+         console.log("Sonntag");
+        
+    }
+    if (wochentag == "Sonntag") {
+         console.log("Unsinntag");
+        
+    }
+}
+
+  
+
+nextDay("Montag");
+nextDay("Dienstag");
+nextDay("Mittwoch");
+nextDay("Donnerstag");
+nextDay("Freitag");
+nextDay("Samstag");
+nextDay("Sonntag");
+nextDay("Unsinntag");
+
