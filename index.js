701d966ff1f80948dd1799a6e5e85f825c9d1eab
Mazen <mazen.taha88@gmail.com>
2021-11-24 14:36:24 +0100

    My lösung

---
+++ index.js
@@ -0,0 +1,26 @@
+// const Wochentage [ "Montag", "Dienstag" , "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+
+function nextDay(Wochentag) {
+
+    if(Wochentag == "Montag");
+    console.log("Dienstag");
+
+    if(Wochentag == "Mittwoch");
+    console.log("Donnerstag");
+
+}
+    
+function nextDay(Wochentag) {
+
+    if(Wochentag == "Spaßtag");
+    console.log("Unsinnstag");
+}
+
+
+
+nextDay("Montag");
+// nextDay("Dienstag");
+
+nextDay("Mittwoch");
+
+nextDay("Spaßtag");
