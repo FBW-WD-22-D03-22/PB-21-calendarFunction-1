8541353f8602c73555b663b2a08bd17ce2a4d0c0
Serdar68 <Serdarbalkis@outlook.de>
2021-11-24 13:30:30 +0100

    fertig

---
+++ wochentag.js
@@ -0,0 +1,35 @@
+function woche(montag,dienstag,mittwoch,donnerstag,freitag,samstag,sonntag, Spaßtag) {
+    sonntag=true;
+    if(sonntag){
+        console.log("Montag")
+    }
+      montag=true;
+      if (montag) {
+          console.log("Dienstag")
+      }
+      dienstag=true;
+      if (dienstag) {
+          console.log("Mittwoch")
+      }
+      mittwoch=true;
+      if (mittwoch) {
+          console.log("Donnerstag")
+      }
+      donnerstag=true;
+      if (donnerstag) {
+          console.log("Freitag")
+      }
+      freitag=true;
+      if (freitag) {
+          console.log("Samstag")
+      }
+      samstag=true;
+      if (samstag) {
+          console.log("Sonntag")
+      }
+  }
+  Spaßtag=true;
+  if (Spaßtag) {
+      console.log("Unsinntag")
+  }
+  woche();
