513ee6ee287ca3da43205ac3e807e032d7dfc055
sprotte <metatron1972@live.de>
2021-11-24 14:37:18 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,28 @@
+function nachsterTag(wochentag) {
+    if(wochentag == "Montag"){
+    console.log("Dienstag");
+}
+if(wochentag == "Dienstag"){
+    console.log("Mittwoch");    
+if(wochentag == "Mittwoch"){
+    console.log("Donerstag");
+if(wochentag == "Donerstag"){
+    console.log("Freitag");
+if(wochentag == "Freitag"){
+    console.log("Samstag")
+if(wochentag == "Samstag"){
+    console.log("Sonntag")
+} 
+}    
+}    
+}    
+}
+}
+
+nachsterTag("Montag"); 
+nachsterTag("Dienstag"); 
+nachsterTag("Mittwoch");
+nachsterTag("Donerstag");
+nachsterTag("Freitag");
+nachsterTag("Samstag");
+nachsterTag("Sonntag"); 
