a058e591ed4cf3d6c61353cac21f0bd628e9beb3
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-24 14:15:58 +0100

    js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function nextTag(tag){
+    if (tag=="Montag" ){
+        console.log("Dienstag");
+    } else if(tag=="Dienstag") {
+        console.log("Mittwoch");
+    }else if(tag=="Mittwoch") {
+        console.log("Donnerstag");
+    }else if(tag=="Donnerstag") {
+        console.log("Freitag");
+    }else if(tag=="Freitag") {
+        console.log("Samstag");
+    }else if(tag=="Samstag") {
+        console.log("Sonntag");
+     } else if(tag=="Sonntag") {
+            console.log("Unsinnstag");
+
+     }
+    }
+
+nextTag("Montag");
+nextTag("Dienstag");
+nextTag("Mittwoch");
+nextTag("Donnerstag");
+nextTag("Freitag");
+nextTag("Samstag");
+nextTag("Sonntag");
+nextTag("Unsinnstag");
