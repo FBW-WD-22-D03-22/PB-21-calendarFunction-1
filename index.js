e213fde856e3b0619478d2598c2d97a11cb0305c
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-24 14:30:01 +0100

    fertig

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-function nextTag(tag){
+function nextDay(tag){
     if (tag =="Montag" ){
         console.log("Dienstag");
     } 
     if(tag == "Dienstag") {
         console.log("Mittwoch");
     }
-   if(tag == "Mittwoch") {
+    if(tag == "Mittwoch") {
         console.log("Donnerstag");
     }
-   if(tag == "Donnerstag") {
+    if(tag == "Donnerstag") {
         console.log("Freitag");
     }
-   if(tag == "Freitag") {
+    if(tag == "Freitag") {
         console.log("Samstag");
     }
-   if(tag == "Samstag") {
+    if(tag == "Samstag") {
         console.log("Sonntag");
      } 
-   if(tag == "Sonntag") {
+    if(tag == "Sonntag") {
         console.log("Unsinnstag");
 
      }
     }
-
-nextTag("Montag");
-nextTag("Dienstag");
-nextTag("Mittwoch");
-nextTag("Donnerstag");
-nextTag("Freitag");
-nextTag("Samstag");
-nextTag("Sonntag");
-nextTag("Unsinnstag");
+nextDay("Montag");
+nextDay("Dienstag");
+nextDay("Mittwoch");
+nextDay("Donnerstag");
+nextDay("Freitag");
+nextDay("Samstag");
+nextDay("Sonntag");
+nextDay("Unsinnstag");
+    
