7be6b239d23aaadfded6c87f90de8940eaff4297
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-24 13:54:54 +0100

    First push

---
+++ index.js
@@ -0,0 +1,33 @@
+function nächsterTag(a, b, c, d, e, f, g){
+    
+}
+
+
+
+
+function wochentage( a, b, c, d, e, f, g) {
+    if(a){
+        return (b)}
+    if (b){
+        return (c)}
+    if(c){
+        return (d)}
+    if(d){
+        return (e)}
+    if(e){
+        return(f)}
+    if(f){
+        return(g)}
+    if(g)
+        return("Spaßtag")
+    else{
+        console.log("Hallo")
+    }
+}
+
+
+
+
+wochentage("Montag");
+
+
