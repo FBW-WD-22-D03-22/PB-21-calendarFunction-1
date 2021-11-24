2fc4cec6a9db3021960dcd1ce9a41b0fe1aa9474
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-24 14:22:09 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,33 @@
+function nextTag(tag){
+    if (tag =="Montag" ){
+        console.log("Dienstag");
+    } 
+    if(tag == "Dienstag") {
+        console.log("Mittwoch");
+    }
+   if(tag == "Mittwoch") {
+        console.log("Donnerstag");
+    }
+   if(tag == "Donnerstag") {
+        console.log("Freitag");
+    }
+   if(tag == "Freitag") {
+        console.log("Samstag");
+    }
+   if(tag == "Samstag") {
+        console.log("Sonntag");
+     } 
+   if(tag == "Sonntag") {
+        console.log("Unsinnstag");
+
+     }
+    }
+
+nextTag("Montag");
+nextTag("Dienstag");
+nextTag("Mittwoch");
+nextTag("Donnerstag");
+nextTag("Freitag");
+nextTag("Samstag");
+nextTag("Sonntag");
+nextTag("Unsinnstag");
