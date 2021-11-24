8c50714baa4409b07720d9fb23643350071040d3
slavablack <veaceslav.ciornii@magenta.de>
2021-11-24 14:16:20 +0100

    v2

--- calendar.js
+++ calendar.js
@@ -1,35 +1,78 @@
-function woche(montag,dienstag,mittwoch,donnerstag,freitag,samstag,sonntag, Spaßtag) {
-  sonntag=true;
-  if(sonntag){
-      console.log("Montag")
-  }
-    montag=true;
-    if (montag) {
+ function woche(montag,dienstag,mittwoch,donnerstag,freitag,samstag,sonntag, Spaßtag) {
+   sonntag=true;
+   if(sonntag){
+       console.log("Montag")
+   }
+     montag=true;
+     if (montag) {
+         console.log("Dienstag")
+     }
+     dienstag=true;
+     if (dienstag) {
+         console.log("Mittwoch")
+     }
+     mittwoch=true;
+     if (mittwoch) {
+         console.log("Donnerstag")
+     }
+     donnerstag=true;
+     if (donnerstag) {
+         console.log("Freitag")
+     }
+     freitag=true;
+     if (freitag) {
+         console.log("Samstag")
+     }
+     samstag=true;
+     if (samstag) {
+         console.log("Sonntag")
+     }Spaßtag=true;
+ if (Spaßtag) {
+     console.log("Unsinntag")
+ }
+
+ }
+ woche();
+
+function name(Sonntag) {
+    if (name==Sonntag) {
+        console.log("Montag")
+    }
+    
+}
+console.log("---------------------------");
+function Week(Weekday) {
+    if (Weekday=="Sonntag") {
+        console.log("Montag")
+    }
+    if (Weekday=="Montag") {
         console.log("Dienstag")
     }
-    dienstag=true;
-    if (dienstag) {
-        console.log("Mittwoch")
+    if (Weekday=="Dienstag") {
+        console.log("Mitwoch")
     }
-    mittwoch=true;
-    if (mittwoch) {
+    if (Weekday=="Mitwoch") {
         console.log("Donnerstag")
     }
-    donnerstag=true;
-    if (donnerstag) {
+    if (Weekday=="Donnerstag") {
         console.log("Freitag")
     }
-    freitag=true;
-    if (freitag) {
+    if (Weekday=="Freitag") {
         console.log("Samstag")
     }
-    samstag=true;
-    if (samstag) {
+    if (Weekday=="Samstag") {
         console.log("Sonntag")
     }
+    if (Weekday=="Spaßtag") {
+        console.log("Unsinntag")
+    }
+    
 }
-Spaßtag=true;
-if (Spaßtag) {
-    console.log("Unsinntag")
-}
-woche();
+Week("Sonntag");
+Week("Montag");
+Week("Dienstag");
+Week("Mitwoch");
+Week("Donnerstag");
+Week("Freitag");
+Week("Samstag");
+Week("Spaßtag");
