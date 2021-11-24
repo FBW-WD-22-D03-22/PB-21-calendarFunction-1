7f099d8248798ce2411688451bba50039eccc874
sham912411 <shamalhakim91@gmail.com>
2021-11-24 14:13:31 +0100

    lösung

---
+++ calendar.js
@@ -0,0 +1,22 @@
+function woche (montag,dienstag,mittwoch,donnerstag,freitag,samstag,sonntag,  Spaßtag) {
+    montag = true;
+    if (montag) {
+    console ("Dienstag");
+    }
+    
+}
+mittwoch=true;
+if (mittwoch) {
+    console.log("Donnerstag")
+}
+Spaßtag=true;
+  if (Spaßtag) {
+      console.log("Unsinntag")
+  }
+  
+
+
+  function nextDay(wochentag) {
+    if(wochentag == "Montag"){
+            console.log("Dienstag");
+    }
